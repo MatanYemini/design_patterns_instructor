95bfb11204160202d684405263143c66fc530ac2
Yemini <yeminimatan@gmail.com>
2020-03-24 19:20:02 +0200

    Adding Builder Design Pattern

---
+++ FunctionalBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunctionalBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
