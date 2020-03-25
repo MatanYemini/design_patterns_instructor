06a032c2e0cc9533a9d13ce782500eee633058dc
Yemini <yeminimatan@gmail.com>
2020-03-25 12:22:04 +0200

    Adding Adapter

---
+++ Adapter_YeminiEx/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Adapter_YeminiEx
+{
+   
+    public class Square
+    {
+        public int Side;
+    }
+
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        private readonly Square _square;
+        public SquareToRectangleAdapter(Square square)
+        {
+            this._square = square;
+        }
+
+        public int Width => _square.Side;
+        public int Height => _square.Side;
+    }
+}
+
